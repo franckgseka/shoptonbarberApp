45c6f3c1fb07386364822325a90d8de3dc590d6b
franckgseka <franckgseka@gmail.com>
2022-11-27 17:00:43 +0100

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <Image 
+        style = {{ width:300, height: 300 }}
+        source = {require('./assets/shoptbarberlogo.png')}
+      />
+
+      <Text>ShopTonBarber !</Text>
     </View>
   );
 }
@@ -13,7 +19,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F6E7D8',
     alignItems: 'center',
     justifyContent: 'center',
   },
